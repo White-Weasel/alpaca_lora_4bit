2d38ea01fc4148f7bb8f92feadcf77db62be99e1
Binh Giang <ndbghdvn@gmail.com>
2023-09-05 14:08:40 +0700

    test inference

--- inference.py
+++ inference.py
@@ -1,16 +1,20 @@
 import time
 import torch
-from alpaca_lora_4bit.autograd_4bit import load_llama_model_4bit_low_ram, Autograd4bitQuantLinear
+from alpaca_lora_4bit.autograd_4bit import load_llama_model_4bit_low_ram, load_llama_model_4bit_low_ram_and_offload, Autograd4bitQuantLinear
 from alpaca_lora_4bit.monkeypatch.peft_tuners_lora_monkey_patch import replace_peft_model_with_int4_lora_model
 replace_peft_model_with_int4_lora_model()
 
-config_path = './llama-13b-4bit/'
-model_path = './llama-13b-4bit.pt'
+config_path = '/tmp/text-generation-webui/models/Neko-Institute-of-Science_LLaMA-13B-4bit-128g/'
+model_path = '/tmp/text-generation-webui/models/Neko-Institute-of-Science_LLaMA-13B-4bit-128g/llama-13b-4bit-128g.safetensors'
+lora_path = '/content/gdrive/MyDrive/ColabNotebooks/Lora/Salie/adapter_model.bin'
 
 # For test lora, Use this:
-# load_llama_model_4bit_low_ram_and_offload(config_path, model_path, lora_path=None, groupsize=-1, seqlen=2048, max_memory=None, is_v1_model=False, bits=4)
+model, tokenizer = load_llama_model_4bit_low_ram_and_offload(config_path, model_path, lora_path=lora_path,
+                                                             groupsize=128, seqlen=2048, bits=4,
+                                                             max_memory={0: '15Gib', 'cpu': '12Gib'},
+                                                             is_v1_model=True)
 
-model, tokenizer = load_llama_model_4bit_low_ram(config_path, model_path, groupsize=-1)
+# model, tokenizer = load_llama_model_4bit_low_ram(config_path, model_path, groupsize=-1)
 
 print('Fitting 4bit scales and zeros to half')
 model.half()
@@ -26,7 +30,18 @@ from amp_wrapper import AMPWrapper
 wrapper = AMPWrapper(model)
 wrapper.apply_generate()
 
-prompt = '''I think the meaning of life is'''
+prompt = '''As Salie, response to the following conversation.
+
+### Conversation:
+@Westsidebill: Much better, sleeping better, more energy. I did gain about 15 lbs.
+@durbblurb:  I did gain about 15 lbs.
+That part is not always mentioned.
+@sleeps_too_little: Smoking used to be advertised as a weight loss method apparently
+@lionx0x: Makes sense. I dropped 30 pounds in a few months last summer from smoking. Looked great felt like shit though lol.
+@Salie:
+
+### Response:
+'''
 batch = tokenizer(prompt, return_tensors="pt", add_special_tokens=False)
 batch = {k: v.cuda() for k, v in batch.items()}
 
@@ -35,7 +50,7 @@ with torch.no_grad():
     generated = model.generate(inputs=batch["input_ids"],
                                do_sample=True, use_cache=True,
                                repetition_penalty=1.1,
-                               max_new_tokens=20,
+                               max_new_tokens=500,
                                temperature=0.9,
                                top_p=0.95,
                                top_k=40,
